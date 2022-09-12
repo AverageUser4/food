32a0d0d8d5893d2cacc1f6bff2f8b8bb33e9b907
Kacper <kacpersloka@o2.pl>
2022-09-12 16:14:05 +0200

    bad

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ function populateHeaderList() {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Food.com</title>
   <link rel="stylesheet" href="css/main.css">
   <script type="module" href="js/main.js"></script>
 
@@ -94,6 +94,51 @@ function populateHeaderList() {
     </nav>
 
   </header>
+
+  <main class="the-main">
+
+    <section class="the-main-first-section">
+
+      <a class="first-section__top-link" href="javascript:void(0)">
+        <img class="first-section__top-link-image" src="resources/images/carousel/for-kids.jpg">
+        <span class="first-section__top-link-span">Top 100 Recipes For Kids</span>
+      </a>
+
+      <a class="first-section__top-link" href="javascript:void(0)">
+        <img class="first-section__top-link-image" src="resources/images/carousel/pork-chops.jpg">
+        <span class="first-section__top-link-span">Pork Chops, 34 Ways</span>
+      </a>
+
+      <a class="first-section__top-link" href="javascript:void(0)">
+        <img class="first-section__top-link-image" src="resources/images/carousel/side-dishes.jpg">
+        <span class="first-section__top-link-span">50 Ultimate Side Dishes</span>
+      </a>
+
+      <article class="first-section-whats-fresh">
+
+        <h3 class="whats-fresh__heading" >What's fresh?</h3>
+
+        <div class="whats-fresh__content">
+
+          <article class="whats-fresh__post">
+
+            <div class="whats-fresh__post-icon-container">
+
+              <img class="whats-fresh__post-icon-image" src="resources/images/icons/person-purple.svg">
+
+            </div>
+
+          </article>
+
+        </div>
+
+        <a class="whats-fresh__view-all" href="javascript:void(0)">View all</a>
+
+      </article>
+
+    </section>
+
+  </main>
 
 </body>
 
